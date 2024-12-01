26831cb8f49c7605d0b031e31147376a67e101b8
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-12-01 17:59:24 +0100

    Finsih the lab

--- src/main/java/com/example/productmanagementsystem/ProductServlet.java
+++ src/main/java/com/example/productmanagementsystem/ProductServlet.java
@@ -2,27 +2,33 @@ package com.example.productmanagementsystem;
 
 import java.io.*;
 
+import com.example.productmanagementsystem.utils.Product;
+import com.example.productmanagementsystem.utils.ProductList;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+@WebServlet(name = "ProductServlet", urlPatterns = {"/ProductServlet"})
+public class ProductServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String productName = request.getParameter("productName");
+        int quantity;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
-    private String message;
+        try {
+            quantity = Integer.parseInt(request.getParameter("quantity"));
+        } catch (NumberFormatException e) {
+            response.getWriter().write("<h1>Error: Quantity must be a valid number.</h1>");
+            return;
+        }
 
-    public void init() {
-        message = "Hello World!";
-    }
+        ProductList productList = new ProductList();
+        Product product = productList.findByName(productName);
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void destroy() {
+        if (product != null) {
+            double totalAmountTTC = quantity * product.getPriceExcludingTax() * (1 + product.getVAT());
+            response.getWriter().write("<h1>Total Cost (TTC): " + totalAmountTTC + "</h1>");
+        } else {
+            response.getWriter().write("<h1>Product not found.</h1>");
+        }
     }
 }
